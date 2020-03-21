e136408722605a2ed408606f67f2cae91422ee32
Pray Chow <1007950881@qq.com>
2020-03-22 00:08:32 +0800

    Ali_2

--- Ali_2.java
+++ Ali_2.java
@@ -1,9 +1,25 @@
 public class Ali_2 {
+    /*
+        给一个音符序列，求能组成上升序列的最大长度，每个序列都是上升序列
+        INPUT :
+            n 序列个数 [1, 1e6]
+            n 个字符串序列 [1, 100]
+        OUTPUT :
+            最长上升序列长度
+        example :
+            IN : 4
+                 aaa bcd zzz bcdef
+            OUT: 11 (aaabcdefzzz)
+        idea : 观察序列 ab 和 aaabbbbb，这两个在生成序列时对于其他序列的影响是一样的，不过序列长度不同，所以可以将
+               所有的 1e6 个序列缩减至 26*26 大小的 arr[i][j]，表示从字母 i 到 j 的最长序列长，特殊注意 arr[i][i]
+               需要将所有序列求和。
+     */
 
     final int SIZE = 26;
     int[][] arr = new int[SIZE][SIZE];
     int[] dp = new int[SIZE];
 
+    // 预处理至 arr 二维数组中，数值取最长的，对角线特殊处理
     void preProcessing(String[] str) {
         for (String s : str) {
             int start = s.charAt(0) - 'a';
@@ -15,6 +31,8 @@ public class Ali_2 {
         }
     }
 
+    // 从尾至首，dp[i] 表示从字母 i+'a' 开始到字母 z 的最长序列。每次搜索 arr[i] 一行不为 0 的和不放 arr[i] 开头的
+    // 取最大值为 dp[i]。dp[i] = max{arr[i][j] + dp[j] + arr[i][i] (j in [i+1, SIZE-1]), arr[i][i] + dp[i + 1]}.
     int sequenceLIS(String[] str) {
         preProcessing(str);
         dp[SIZE - 1] = arr[SIZE - 1][SIZE - 1];
@@ -36,11 +54,9 @@ public class Ali_2 {
         System.out.println(ali.sequenceLIS(str));
         for (int i = 0; i < ali.SIZE; ++i) {
             System.out.print((char) ('a' + i) + "\t");
-        }
-        System.out.println();
+        } System.out.println();
         for (int temp : ali.dp) {
             System.out.print(temp + "\t");
-        }
-        System.out.println();
+        } System.out.println();
     }
 }
