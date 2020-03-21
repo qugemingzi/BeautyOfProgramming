6f1d959dcace9f64a1062f4c8bb961e62b388b0e
Pray Chow <1007950881@qq.com>
2020-03-22 00:11:03 +0800

    Meituan_1

--- Meituan_1.java
+++ Meituan_1.java
@@ -3,14 +3,35 @@ import java.util.Queue;
 import java.util.Scanner;
 
 public class Meituan_1 {
+    /*
+        扎银花
+        每人一堆牌，每人挑选最大的三张牌比较和，输出最大的和
+        INPUT :
+            n 双方掌握的牌的数量, [1, 20000]
+            两行，每行有 n 个数字，分别代表双方可选的 n 张牌, [0, 1e9]
+        example :
+        IN : 5
+             1 2 3 4 5
+             1 2 3 4 6
+        OUT: 13
+        idea :
+            最大的 k 个数 (参考 BiggestK.java 《编程之美》)
+            分别比较即可，注意结果类型，3 个 1e9 会超 int 范围。
+     */
 
+    // 采用最小堆实现
     static int[] KBig(int[] arr, int k){
         if(k <= 0){ return new int[0]; }
         if(arr.length <= k){ return arr; }
-        Queue<Integer> queue = new PriorityQueue(k);
-        for(int i = 0; i < k; ++i){
+        Queue<Integer> queue = new PriorityQueue(k); // 默认最小堆
+//        Queue<Integer> maxqueue = new PriorityQueue<>(new Comparator<Integer>() {
+//            @Override
+//            public int compare(Integer i1, Integer i2) {
+//                return i2 - i1;
+//            }
+//        }); // 构造最大堆
+        for(int i = 0; i < k; ++i)
             queue.offer(arr[i]);
-        }
         for(int i = k; i < arr.length; ++i){
             if(arr[i] > queue.peek()){
                 queue.poll();
@@ -18,9 +39,8 @@ public class Meituan_1 {
             }
         }
         int[] res = new int[k];
-        for(int i = k-1; i >= 0; --i){
+        for(int i = k-1; i >= 0; --i)
             res[i] = queue.poll();
-        }
         return res;
     }
 
@@ -35,6 +55,7 @@ public class Meituan_1 {
         for(int i = 0; i < n; ++i){
             b[i] = scan.nextInt();
         }
+        // 结果可能是 long 类型
         long a_res = 0, b_res = 0;
         for(int temp : KBig(a, 3)){
             a_res += temp;
