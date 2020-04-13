6039f1958890ce4ad76586e936512e928188fcbd
Pray Chow <1007950881@qq.com>
2020-04-13 23:13:04 +0800

    Package01

--- Package01.java
+++ Package01.java
@@ -5,15 +5,18 @@ package algorithm;
  * 0-1背包
  * 给定一个可装重量 W 的背包和 N 个物品，每个物品有重量和价值两个属性。第 i 个
  * 物品的重量为 wt[i]，价值为 val[i]，用该背包装物品，最多能装的价值为？
- * N = 3, W = 4
- * wt = [2, 1, 2]
- * val = [4, 2, 1]
- * 算法返回 6
+ * INPUT :
+ *      N = 3, W = 4
+ *      wt = [2, 1, 2]
+ *      val = [4, 2, 1]
+ * OUTPUT :
+ *      6
  * idea :
  *      dp[i][w]表示前 i 个物品，重量为 w 情况下最大价值
  *      dp[i][w] = max { dp[i - 1][w],
  *                       dp[i - 1][w - wt[i - 1]] + val[i - 1] }
  *      dp[0][...] = dp[...][0] = 0
+ *      目标即为求 dp[N][W]
  */
 public class Package01 {
 
